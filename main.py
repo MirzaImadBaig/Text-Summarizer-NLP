748936bdbc52ab1e86b6848b22b90dda6e3d496f
Mirza Imad Moiz Baig <mirzaimadbaig871@gmail.com>
2024-07-17 22:47:40 +0530

    data validation done

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
 
 from src.Text_Summarizer.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
+from src.Text_Summarizer.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
+
 from src.Text_Summarizer.logging import logger
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,3 +15,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+    
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e    
--- src/Text_Summarizer/config/configuration.py
+++ src/Text_Summarizer/config/configuration.py
@@ -2,7 +2,8 @@
 
 from src.Text_Summarizer.constants import *                                    # import the file paths stored in 'constants' folder in src
 from src.Text_Summarizer.utils.common import read_yaml, create_directories       # import those util functions 
-from src.Text_Summarizer.entity import (DataIngestionConfig)
+from src.Text_Summarizer.entity import (DataIngestionConfig,
+                                        DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -28,5 +29,19 @@ class ConfigurationManager:
             local_data_file=config.local_data_file,
             unzip_dir=config.unzip_dir 
         )
-
         return data_ingestion_config
+    
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+        return data_validation_config   
+    
+     
