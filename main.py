1ae42bc86bcc0e696ccabab438c2d71db5a33c35
Mirza Imad Moiz Baig <mirzaimadbaig871@gmail.com>
2024-07-18 11:55:04 +0530

    done

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 from src.Text_Summarizer.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from src.Text_Summarizer.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
+from src.Text_Summarizer.pipeline.stage_3_data_transformation import DataTransformationTrainingPipeline
+from src.Text_Summarizer.pipeline.stage_4_model_trainer import ModelTrainerTrainingPipeline
+from src.Text_Summarizer.pipeline.stage_5_model_evaluation import ModelEvaluationTrainingPipeline
 
 from src.Text_Summarizer.logging import logger
 
@@ -27,3 +30,39 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e    
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e    
+    
+
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/Text_Summarizer/pipeline/stage_3_data_transformation.py
@@ -0,0 +1,14 @@
+from src.Text_Summarizer.config.configuration import ConfigurationManager
+from src.Text_Summarizer.conponents.data_transformation import DataTransformation
+from src.Text_Summarizer.logging import logger
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
